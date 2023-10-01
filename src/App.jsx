24096b5d1465fb381627dcaa1b13ec4c0848923a
Emanuel Escudero <escudero.mdz@gmail.com>
2023-10-01 04:47:27 -0300

    useEffect Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY });
+    };
+    /* Si el enable es true, me suscribo al evento */
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove);
+    }
+    /* Cleanup: Desuscribirse para que no se siga ejecutando */
+    return () => {
+      window.removeEventListener('pointermove', handleMove);
+    };
+  }, [enabled]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <div className='app'>
+        <div
+          className='ball'
+          style={{
+            transform: `translate(${position.x}px,${position.y}px)`,
+          }}></div>
+        <h1>Mouse Follower Project</h1>
+        <h2>[ Vite + React ]</h2>
+
+        <a href='https://vitejs.dev' target='_blank' rel='nonopener noreferrer'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href='https://react.dev' target='_blank' rel='nonopener noreferrer'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+
+        <div>
+          <button onClick={() => setEnabled(!enabled)}>
+            {enabled ? 'Disable' : 'Enable'}
+          </button>
+        </div>
+        <p className='read-the-docs'>
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
